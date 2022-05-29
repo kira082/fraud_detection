1836054947c1559a493861877f4ca2c9003723ae
kira082 <vijay.bhavesh082@gmail.com>
2022-05-29 23:18:16 +0530

    add the  2 machine learning model

--- model.py
+++ model.py
@@ -1,1 +1,8 @@
 print("adding the machine learning model code here")
+
+
+def model1():
+    print("adding code for machine learning model1")
+
+def model2():
+    print("adding code for model learning model2")
